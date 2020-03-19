442a9c0fbb5f439b054ea2c96bcf5b32deef5b46
godfella777 <ybenhaim777@gmail.com>
2020-03-19 23:06:50 +0200

    added glide dependency and cover image to the course detail activitiy.
    Seperated java files into different activities

--- app/src/main/java/com/gamedev/codetube/ui/CourseDetailActivity.java
+++ app/src/main/java/com/gamedev/codetube/ui/CourseDetailActivity.java
@@ -3,11 +3,13 @@ package com.gamedev.codetube.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.gamedev.codetube.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.w3c.dom.Text;
 
@@ -15,6 +17,7 @@ public class CourseDetailActivity extends AppCompatActivity {
 
      ImageView CourseThumb, CourseCoverImg;
      TextView tv_title,tv_description;
+     private FloatingActionButton play_fab;
 
 
     @Override
@@ -30,6 +33,7 @@ public class CourseDetailActivity extends AppCompatActivity {
     }
 
     void iniViews(){
+        play_fab = findViewById(R.id.play_fab);
         String movie = getIntent().getExtras().getString("title");
         int imageResource = getIntent().getExtras().getInt("imgURL");
         int imageCover = getIntent().getExtras().getInt("imgCover");
@@ -37,8 +41,13 @@ public class CourseDetailActivity extends AppCompatActivity {
         Glide.with(this).load(imageResource).into(CourseThumb);
         CourseThumb.setImageResource(imageResource);
         tv_title = findViewById(R.id.detail_course_title);
+        tv_title.setText(movie);
+        getSupportActionBar().setTitle(movie);
         tv_description = findViewById(R.id.detail_course_description);
         CourseCoverImg = findViewById(R.id.detail_course_cover);
         Glide.with(this).load(imageCover).into(CourseCoverImg);
+        //set animations
+        CourseCoverImg.setAnimation(AnimationUtils.loadAnimation(this,R.anim.scale_animation));
+        play_fab.setAnimation(AnimationUtils.loadAnimation(this,R.anim.scale_animation));
     }
 }
